cf71c29cc12916693236a6017437c782b7d8de36
gortatka <gtatsiana@tut.by>
2021-02-07 23:38:00 +0300

    add ContactList.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import './App.css';
-import Contact from './components/Contact';
 
+import ContactList from './components/ContactList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Contact
-       avatar="https://randomuser.me/api/portraits/women/75.jpg"
-      name="Vivan Kuhn"
-     online={true}
-     />
-     <Contact
-     avatar="https://randomuser.me/api/portraits/men/55.jpg"
-     name="Michael George"
-    online={false}
-      />
-      <Contact
-     avatar="https://randomuser.me/api/portraits/women/58.jpg"
-     name="Hazel Woods"
-    online={true}/>
+    <ContactList/>
     </div>
   );
 }
